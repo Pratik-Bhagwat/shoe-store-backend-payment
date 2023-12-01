1a9067079cf4ec76333a40eebf2332822e8bbab0
Pratik Bhagwat <pratikbhagwatdev@gmail.com>
2023-12-01 16:19:28 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
+app.use(express.json());
+app.use(cors());
+
+app.post("/api/create-checkout-session", async (req, res) => {
+  const { products } = req.body;
+
+  const lineItems = products.map((product) => ({
+    price_data: {
+      currency: "inr",
+      product_data: {
+        name: product.name,
+      },
+      unit_amount: product.price * 100,
+    },
+    quantity: product.quantity,
+  }));
+
+  const session = await stripe.checkout.sessions.create({
+    line_items: lineItems,
+    mode: "payment",
+    success_url: "http://localhost:5173/success",
+    cancel_url: "http://localhost:5173/cancel",
+  });
+  res.json({ id: session.id });
+});
+
+const port = (process.env.PORT || 8000, "0.0.0.0");
